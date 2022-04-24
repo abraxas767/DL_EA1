e52a39fd97c00afe1addd1dfa9fe9feff88c6ab0
Abraxas767 <Chrs.kaiser3@live.de>
2022-04-24 19:06:24 +0200

    includes routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import MainContainer from './container/MainContainer';
 import DocContainer from './container/DocContainer';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return(
-    <DocContainer />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/ml5" element={<MainContainer />}/>
+        <Route path="/doc" element={<DocContainer />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
